c222f2fcd79b30e902af3fee70866bc0bb623e91
soldiermakk <liangxia1998@outlook.com>
2019-07-13 16:20:27 +0800

    代码优化

--- js/main.js
+++ js/main.js
@@ -1,21 +1,38 @@
-$('.images > img:nth-child(1)').addClass('current')
-$('.images > img:nth-child(2)').addClass('wait')
-$('.images > img:nth-child(3)').addClass('wait')
-var n = 1
+var n
+begin.call()
 setInterval(function () {
-    $(`.images > img:nth-child(${x(n)})`).removeClass('current wait').addClass('leave')
+
+    makeLeave.call(undefined, getImage(n))
         .one('transitionend', function (e) {
-            $(e.currentTarget).removeClass('current leave').addClass('wait')
+            makeWait.call(undefined, $(e.currentTarget))
         })
     console.log(x(n))
-    $(`.images > img:nth-child(${x(n + 1)})`).removeClass('leave wait').addClass('current')
+    makeCurrent.call(undefined, getImage(n + 1))
     //下一个循环开始
     n = n + 1
     n = x(n)
 }, 3000)
+function getImage(n) {
+    return $(`.images > img:nth-child(${x(n)})`)
+}
+function makeWait($node) {
+    return $node.removeClass('current leave').addClass('wait')
+}
+function makeCurrent($node) {
+    return $node.removeClass('leave wait').addClass('current')
+}
+function makeLeave($node) {
+    return $node.removeClass('current wait').addClass('leave')
+}
 function x(n) {
     if (n > 3) {
         n = n - 3
     }
     return n
+}
+function begin() {
+    $('.images > img:nth-child(1)').addClass('current')
+    $('.images > img:nth-child(2)').addClass('wait')
+    $('.images > img:nth-child(3)').addClass('wait')
+    n = 1
 }
