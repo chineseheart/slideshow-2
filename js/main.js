728feed1d81a92acbef52b2c05341eac3e8447c4
soldiermakk <liangxia1998@outlook.com>
2019-07-13 15:46:52 +0800

    更新

--- js/main.js
+++ js/main.js
@@ -1,19 +1,21 @@
-$('images > img:nth-child(1)').addClass('current')
-$('images > img:nth-child(2)').addClass('wait')
-$('images > img:nth-child(3)').addClass('wait')
+$('.images > img:nth-child(1)').addClass('current')
+$('.images > img:nth-child(2)').addClass('wait')
+$('.images > img:nth-child(3)').addClass('wait')
 var n = 1
 setInterval(function () {
-    $(`images > img:nth-child(${x(n)})`).addClass('leave').removeClass('current wait')
-        .one('transtionend', function (e) {
-            $(e.currentTarget).addClass('wait').removeClass('current leave')
+    $(`.images > img:nth-child(${x(n)})`).removeClass('current wait').addClass('leave')
+        .one('transitionend', function (e) {
+            $(e.currentTarget).removeClass('current leave').addClass('wait')
         })
-    $(`images > img:nth-child(${x(n + 1)})`).addClass('current').removeClass('leave wait')
+    console.log(x(n))
+    $(`.images > img:nth-child(${x(n + 1)})`).removeClass('leave wait').addClass('current')
     //下一个循环开始
-    n += 1
+    n = n + 1
+    n = x(n)
 }, 3000)
-function x() {
-    if (n >= 3) {
-        n = n - 2
+function x(n) {
+    if (n > 3) {
+        n = n - 3
     }
     return n
 }
